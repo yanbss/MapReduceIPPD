e7e7e2afabfeb0e5c563a4f8867972b3900e6de7
Yan <iaobs@outlook.com>
2018-01-13 17:57:28 -0200

    Adicionados os imports do Spark

--- src/main/java/InvertedIndex.java
+++ src/main/java/InvertedIndex.java
@@ -1,2 +1,8 @@
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+import scala.Tuple2;
+
 public class InvertedIndex {
 }
